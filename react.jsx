ed40bf78aed87d475c12935c43e0741dfdf364e1
Yang Lu <iamyangandirule@gmail.com>
2017-11-23 16:36:00 -0500

    Add files via upload

---
+++ react.jsx
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
